0f7151627125705b8b8d758fadbfc631fb29acd7
Douglas Floriano <douglas97floriano@gmail.com>
2026-06-02 18:27:11 -0300

    adiciona componentes Card e Typography com estilos CSS e atualiza o layout da secao

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Aside } from './components/Aside';
 import { Container } from './components/Container';
 import { Main } from './components/Main';
 import { SearchInput } from './components/SearchInput';
+import { Typography } from './components/Typography';
+
+import Card from './components/Card';
 
 function App() {
     return (
@@ -9,6 +12,48 @@ function App() {
             <Aside />
             <Main>
                 <SearchInput placeholder={'Procure seu dinheiro...'} />
+                <div>
+                    <Typography variant='h1'>
+                        Olá, Douglas!
+                    </Typography>
+                    <Typography variant='body'>
+                        Veja como estão suas finanças hoje.
+                    </Typography>
+                </div>
+                <section>
+                    <Card>
+                        <Card.Header>
+                            Orçamento diário disponível:
+                        </Card.Header>
+                        <Card.Body>
+                            R$ 200
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            Orçamento diário disponível:
+                        </Card.Header>
+                        <Card.Body>
+                            R$ 200
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            Orçamento diário disponível:
+                        </Card.Header>
+                        <Card.Body>
+                            R$ 200
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            Orçamento diário disponível:
+                        </Card.Header>
+                        <Card.Body>
+                            R$ 200
+                        </Card.Body>
+                    </Card>                                                            
+                </section>
             </Main>
         </Container>
     );
